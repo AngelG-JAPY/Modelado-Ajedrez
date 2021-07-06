43ff83f089f421924f244003f704b69f325ca96e
AngelG-JAPY <angelgarciara16@gmail.com>
2021-07-05 22:17:17 -0500

    Se agregaron los constructores, getter and setter a las clases Ficha, Casilla.

--- src/main/java/co/edu/utp/misiontic2022/c2/Casilla.java
+++ src/main/java/co/edu/utp/misiontic2022/c2/Casilla.java
@@ -2,14 +2,56 @@ package co.edu.utp.misiontic2022.c2;
 
 public class Casilla {
 
-    private String color;
+    private Color color;
     private Integer columna;
     private Integer fila;
     private Ficha ficha;
 
+    public Casilla(){
 
+    }
 
+    public Casilla(Color color, Integer columna, Integer fila, Ficha ficha) {
+        this.color = color;
+        this.columna = columna;
+        this.fila = fila;
+        this.ficha = ficha;
+    }
 
+    
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public Integer getColumna() {
+        return columna;
+    }
+
+    public void setColumna(Integer columna) {
+        this.columna = columna;
+    }
+
+    public Integer getFila() {
+        return fila;
+    }
+
+    public void setFila(Integer fila) {
+        this.fila = fila;
+    }
+
+    public Ficha getFicha() {
+        return ficha;
+    }
+
+    public void setFicha(Ficha ficha) {
+        this.ficha = ficha;
+    }
+
+    //Metodos.
 
     public boolean ocupada(){
         return true;
--- src/main/java/co/edu/utp/misiontic2022/c2/Ficha.java
+++ src/main/java/co/edu/utp/misiontic2022/c2/Ficha.java
@@ -2,7 +2,22 @@ package co.edu.utp.misiontic2022.c2;
 
 public abstract class Ficha {
     
-    private String color;
+    private Color color;
+
+    public Ficha(Color color) {
+        this.color = color;
+    }
+
+    //Getter and Setters.
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    //Métodos.
 
     public abstract boolean comer();
 
